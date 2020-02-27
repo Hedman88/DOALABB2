57409efcfe3bbd9eb5816c67e493407781eea588
Harry Hedman <harhed-9@student.ltu.se>
2020-02-27 15:58:48 +0100

    started, 10 min

---
+++ Part1/Main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "Header.h"
+using namespace std;
+int binarySearch(int arr[], int item, int low, int high) { // code borrowed from https://www.tutorialspoint.com/binary-insertion-sort-in-cplusplus
+	if (high <= low)
+		return (item > arr[low]) ? (low + 1) : low;
+	int mid = (low + high) / 2;
+	if (item == arr[mid])
+		return mid + 1;
+	if (item > arr[mid])
+		return binarySearch(arr, item, mid + 1, high);
+	return binarySearch(arr, item, low, mid - 1);
+}
+void BinaryInsertionSort(int arr[], int n) { // code borrowed from https://www.tutorialspoint.com/binary-insertion-sort-in-cplusplus
+	int i, loc, j, k, selected;
+	for (i = 1; i < n; ++i) {
+		j = i - 1;
+		selected = arr[i];
+		loc = binarySearch(arr, selected, 0, j);
+		while (j >= loc) {
+			arr[j + 1] = arr[j];
+			j--;
+		}
+		arr[j + 1] = selected;
+	}
+}
+int main() {
+	int arr[] = { 12, 56, 1, 67, 45, 8, 82, 16, 63, 23 }; // code borrowed from https://www.tutorialspoint.com/binary-insertion-sort-in-cplusplus
+	int n = sizeof(arr) / sizeof(arr[0]), i;
+	BinaryInsertionSort(arr, n);
+	cout << "Sorted array is : \n";
+	for (i = 0; i < n; i++)
+		cout << arr[i] << "\t";
+	return 0;
+}
